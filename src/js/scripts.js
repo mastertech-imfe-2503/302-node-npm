46263c1cdd4aa8ee51e745f464db375685cad754
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-04-10 22:04:43 -0300

    Add webpack-dev-server and import/export examples

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,6 @@
+import {getRandom} from './utils.js';
+import addFunction from './utils.js';
+
 console.log('Hello world');
+console.log(getRandom(0, 50));
+console.log(addFunction(1, 2));
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,9 @@
-const getRandom = (min, max) => {
+export const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+const add = (a, b) => {
+    return a + b;
+};
+
+export default add;
